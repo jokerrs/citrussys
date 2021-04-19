425ce817574268e696db2d1134b827332d7fb81c
jokerrs <nemanja@joker.rs>
2021-04-19 21:53:53 +0200

    removing the env package, and adding the vanilla loader for .env

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+foreach (file("../.env") as $item) {
+    $config = explode("=", $item);
+    $_ENV[$config[0]] = trim($config[1]);
+}
 require_once "vendor/autoload.php";
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
