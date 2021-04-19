3c4c17a98ded6d1824a5f2743920405d250acd38
jokerrs <nemanja@joker.rs>
2021-04-19 19:08:35 +0200

    adding the singleton connection for the database (mysql)

--- app/DB.php
+++ app/DB.php
@@ -1,6 +1,23 @@
 <?php
 
 namespace App;
+use PDO;
+use PDOException;
+
 class DB
 {
+    protected static PDO $instance;
+    public static function getInstance()
+    {
+        if(empty(self::$instance)){
+            try{
+                self::$instance = new PDO("mysql:host=".$_ENV['DBHOST'].";dbname=".$_ENV['DBNAME'], $_ENV['DBUSERNAME'], $_ENV['DBPASSWORD']);
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+            }catch (PDOException $e){
+                echo $e->getMessage();
+            }
+            return self::$instance;
+        }
+        return "Something went wrong";
+    }
 }
