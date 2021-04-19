ccf79cfce0525ef1e28a19bb958caa8602dd7cdb
jokerrs <nemanja@joker.rs>
2021-04-19 19:56:23 +0200

    Following the PSR standards

--- app/DB.php
+++ app/DB.php
@@ -1,19 +1,29 @@
 <?php
 
 namespace App;
+
 use PDO;
 use PDOException;
 
 class DB
 {
     protected static PDO $instance;
+
+    /**
+     * @return PDO|string
+     */
     public static function getInstance()
     {
-        if(empty(self::$instance)){
-            try{
-                self::$instance = new PDO("mysql:host=".$_ENV['DBHOST'].";dbname=".$_ENV['DBNAME'], $_ENV['DBUSERNAME'], $_ENV['DBPASSWORD']);
+        if (empty(self::$instance)) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=" . $_ENV['DBHOST'] . // DATABASE HOST
+                    ";dbname=" . $_ENV['DBNAME'], //DATABASE NAME
+                    $_ENV['DBUSERNAME'], // DATABASE USERNAME
+                    $_ENV['DBPASSWORD'] // DATABASE PASSWORD
+                );
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-            }catch (PDOException $e){
+            } catch (PDOException $e) {
                 echo $e->getMessage();
             }
             return self::$instance;
