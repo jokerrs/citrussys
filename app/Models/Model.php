5461bdf1362afeaa2184f71a6e2054d80915a0e5
jokerrs <nemanja@joker.rs>
2021-04-20 21:31:23 +0200

    Implementing the all() and where() methods to the Model class

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,9 +6,31 @@ use app\DB;
 
 class Model extends DB implements ModelInterface
 {
-    public function all()
-   {
-       $class = strtolower(substr(get_class($this), 11));
-       return DB::getInstance()->query("SELECT * FROM $class");
+    public function all(): array
+    {
+        $PDO = DB::getInstance()->query("SELECT * FROM ". $this->modelName());
+        if(!$PDO){
+            return ['error' => 'There is no data'];
+        }
+        return $PDO->fetchAll();
+    }
+
+    public function where(string $column, $value, ?int $limit = null): array
+    {
+        $SQL = "SELECT * FROM ". $this->modelName() ." WHERE $column=:$column ";
+        if($limit !== null){
+            $SQL .= " LIMIT " . $limit;
+        }
+        $PDO = DB::getInstance()->prepare($SQL);
+        $PDO->bindValue(":$column", $value);
+        if(!$PDO->execute() || empty($data = $PDO->fetchAll())){
+            return ['error' => 'There is no data'];
+        }
+        return $data;
+    }
+
+    public function modelName(): string
+    {
+        return strtolower(substr(get_class($this), 11));
     }
 }
