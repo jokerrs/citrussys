e6e1863cbd8b1cfbaf9638d5000b1e94a954d59d
jokerrs <nemanja@joker.rs>
2021-04-20 11:16:07 +0200

    adding the check for the POST request_method in Router::get method

--- app/Router.php
+++ app/Router.php
@@ -6,6 +6,9 @@ class Router
 
     public static function get($route, $function): void
     {
+        if($_SERVER['REQUEST_METHOD'] === "POST") {
+            return;
+        }
         $uri = explode('?', $_SERVER['REQUEST_URI']);
         if($uri[0] === $route){
             $function->__invoke();
