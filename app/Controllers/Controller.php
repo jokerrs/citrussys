3ff830a599820694e3c97ba4cedb2f09695ed78f
jokerrs <nemanja@joker.rs>
2021-04-21 00:17:24 +0200

    Adding a new argument for a view

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,7 +6,7 @@ use app\Router;
 
 class Controller
 {
-    public static function view($view,?array $params = null): void
+    public static function view($view, ?array $params = null, ?array $params2 = null): void
     {
         require_once Router::$rootDir ."/views/$view.php";
     }
