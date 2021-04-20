c768d153e10b7189f55758f677c6353519e489a7
jokerrs <nemanja@joker.rs>
2021-04-20 13:23:00 +0200

    Fixing the routing and adding the 404 page

--- routes.php
+++ routes.php
@@ -1,7 +1,18 @@
 <?php
 
+use app\Controllers\Controller;
 use app\Router;
+new Router(__DIR__);
 
-Router::get('/', function (){
-    echo 'Home Page';
-});
+//Router::get('/', [Controller::class, 'testg']);
+//Router::post('/', [Controller::class, 'testp']);
+//Router::get('/test', function (){
+//    print_r(Router::$routes);
+//});
+//Router::get('/testa', function (){
+//    print_r(Router::$routes);
+//});
+
+Router::get('/', 'homepage');
+
+Router::run();
