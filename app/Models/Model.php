0c4d6f18bbccfc88dac0e9125b0864a91d3e96e1
jokerrs <nemanja@joker.rs>
2021-04-20 21:01:24 +0200

    Adding the ModelInterface with a all() function

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,8 +6,9 @@ use app\DB;
 
 class Model extends DB implements ModelInterface
 {
-    public function all(): string
-    {
-        return get_class($this);
+    public function all()
+   {
+       $class = strtolower(substr(get_class($this), 11));
+       return DB::getInstance()->query("SELECT * FROM $class");
     }
 }
