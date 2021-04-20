cd16772b47affdd9bdcc392cd01dad974a0a19a2
jokerrs <nemanja@joker.rs>
2021-04-20 16:56:26 +0200

    include => require_once

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ foreach (file("../.env") as $item) {
     $_ENV[$config[0]] = trim($config[1]);
 }
 require_once "vendor/autoload.php";
-include "functions.php";
+require_once "functions.php";
 require_once "routes.php";
