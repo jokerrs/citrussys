073203005a6f2643a375245594bcdc1a0bee9e4d
jokerrs <nemanja@joker.rs>
2021-04-21 06:56:21 +0200

    Adding a command for creating a database via shell

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 composer install
 read -p "ENTER DATABASE USER NAME: " dbuser
 read -s -p "ENTER DATABASE PASSWORD: " dbpassword
+echo "create database citrussys;" | mysql -u $dbuser -p
 mysql --user=$dbuser --password=$dbpassword citrussys < citrussys.sql
 sed -i "1i DBUSERNAME=$dbuser" .env
 sed -i "2i DBPASSWORD=$dbpassword" .env
