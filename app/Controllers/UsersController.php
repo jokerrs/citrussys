7fa3e7a603784a11e9e9a849c005b3f56bedb026
jokerrs <nemanja@joker.rs>
2021-04-21 17:53:01 +0200

    Adding the logout method

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -6,12 +6,19 @@ use app\Models\Users;
 
 class UsersController extends Controller
 {
-    public static function login(){
+    public static function login()
+    {
         $result = Users::userLogin($_POST['username'], $_POST['password']);
-        if($result['result'] === 'success'){
-            $_SESSION['uid'] =  $result['user_id'];
-        }else{
+        if ($result['result'] === 'success') {
+            $_SESSION['uid'] = $result['user_id'];
+        } else {
             self::view('admin/login', $result);
         }
+    }
+
+    public static function logout()
+    {
+        session_destroy();
+        header("Location: /");
     }
 }
